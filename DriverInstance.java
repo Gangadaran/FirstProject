04eb4ebe7589f2dd9fc83daf5e4ef2f5b6e609b6
Gangadaran S <sgangadaran1999@gmail.com>
2022-09-10 16:17:25 +0530

    changed to 22

--- DriverInstance.java
+++ DriverInstance.java
@@ -15,7 +15,7 @@ public class DriverInstance extends AbstractTestNGCucumberTests {
 	private static final ThreadLocal<WebDriverWait> wait = new  ThreadLocal<WebDriverWait>();
 
 	public void setWait() {
-		wait.set(new WebDriverWait(getDriver(), 20));
+		wait.set(new WebDriverWait(getDriver(), 22));
 	}
 
 	public WebDriverWait getWait() {
